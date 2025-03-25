075203adceff0a8204f9031bc7da88123bdb833f
Pedro Costa <pedro4costa@outlook.com>
2025-03-25 09:50:44 -0300

    Initial commit

---
+++ src/exercicios/Exercicio1/MinhaPrimeiraApp/OlaMundo.java
@@ -0,0 +1,7 @@
+package exercicios.Exercicio1.MinhaPrimeiraApp;
+
+public class OlaMundo {
+    public static void main(String[] args) {
+        System.out.println("Olá, meu nome é Pedro e estou aprendendo Java!");
+    }
+}
